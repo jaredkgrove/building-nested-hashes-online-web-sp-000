4ab44befcf4e38eee5b4f68d7fd8fd8595e3321c
Jared Grove <jaredkgrove@gmail.com>
2019-03-16 16:22:09 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -8,6 +8,10 @@ def fourth_challenge
         {:name => "Benvolio", 
          :age => "17",
          :attitude => "worried"
+        },
+        {:name => "Mercutio", 
+         :age => "18",
+         :attitude => "hot-headed"
         }
       ]
    }, 
